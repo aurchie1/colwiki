be63e92e780a2e3df9f1b0edba8786932fe4a228
Richard Colbert - ubuntu <rich@gmail.com>
2014-02-07 17:38:49 -0700

    second initial commit

---
+++ extensions/NoTitle/NoTitle.i18n.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ *  NoTitle
+ *  Adds a magic word that hides the main title heading in a page
+ *
+ * @file NoTitle.i18n.php
+ * @author Tony Boyles
+ */
+
+$messages = array();
+
+/** English
+ * @author Tony Boyles
+ */
+$messages['en'] = array(
+	'notitle-desc' => 'Adds a magic word to hide the title heading'
+);
+
+/** Message documentation (Message documentation)
+ * @author Shirayuki
+ * @author Tony Boyles
+ */
+$messages['qqq'] = array(
+	'notitle-desc' => '{{desc|name=No Title|url=http://www.mediawiki.org/wiki/Extension:NoTitle}}',
+);
+
+/** Arabic (العربية)
+ * @author Mido
+ */
+$messages['ar'] = array(
+	'notitle-desc' => 'يضيف كلمة سحرية لاخفاء العنوان الرئيسي',
+);
+
+/** Asturian (asturianu)
+ * @author Xuacu
+ */
+$messages['ast'] = array(
+	'notitle-desc' => "Amiesta una pallabra máxica p'anubrir la testera del títulu",
+);
+
+/** German (Deutsch)
+ * @author Metalhead64
+ * @author Purodha
+ */
+$messages['de'] = array(
+	'notitle-desc' => 'Fügt ein magisches Wort zum Verstecken der Titelüberschrift zum Wiki hinzu',
+);
+
+/** Lower Sorbian (dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'notitle-desc' => 'Pśidawa magiske słowo, aby se nadpismo titela schowało',
+);
+
+/** French (français)
+ * @author Gomoko
+ */
+$messages['fr'] = array(
+	'notitle-desc' => 'Ajoute un mot magique pour masquer l’en-tête du titre',
+);
+
+/** Galician (galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'notitle-desc' => 'Engade unha palabra máxica para agochar a cabeceira do título',
+);
+
+/** Upper Sorbian (hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'notitle-desc' => 'Přidawa magiske słowo, zo by so nadpismo titula schowało',
+);
+
+/** Italian (italiano)
+ * @author Beta16
+ */
+$messages['it'] = array(
+	'notitle-desc' => "Aggiunge una parola magica per nascondere il titolo o l'intestazione",
+);
+
+/** Japanese (日本語)
+ * @author Shirayuki
+ */
+$messages['ja'] = array(
+	'notitle-desc' => 'ページ名の見出しを非表示にするマジックワードを追加する',
+);
+
+/** Korean (한국어)
+ * @author 아라
+ */
+$messages['ko'] = array(
+	'notitle-desc' => '제목 머리글을 숨기는 특수 명령을 추가합니다',
+);
+
+/** Colognian (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'notitle-desc' => 'Brängg e Zauberwoot en et Wiki, öm de Övverschreff vun dä Sigg ußzeblände.',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'notitle-desc' => "Setzt e magescht Wuert derbäi fir d'Titeliwwerschrëft ze verstoppen",
+);
+
+/** Macedonian (македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'notitle-desc' => 'Додава волшебен збор за скривање на наслов во заглавието',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'notitle-desc' => 'Voegt een magisch woord toe om de paginanaam te verbergen',
+);
+
+/** Polish (polski)
+ * @author Odie2
+ */
+$messages['pl'] = array(
+	'notitle-desc' => 'Dodaje magiczne słowo, które ukrywa nagłówek tytułowy',
+);
+
+/** tarandíne (tarandíne)
+ * @author Joetaras
+ */
+$messages['roa-tara'] = array(
+	'notitle-desc' => "Aggiunge 'na parola maggeche pe sconnere 'u titole de testate",
+);
+
+/** Tagalog (Tagalog)
+ * @author AnakngAraw
+ */
+$messages['tl'] = array(
+	'notitle-desc' => 'Nagdaragdag ng isang salitang mahiwaga upang maitago ang paulong pamagat',
+);
+
+/** Ukrainian (українська)
+ * @author Base
+ */
+$messages['uk'] = array(
+	'notitle-desc' => 'Додає магічне слово, що приховує заголовок',
+);
+
+/** Simplified Chinese (中文（简体）‎)
+ * @author Yfdyh000
+ */
+$messages['zh-hans'] = array(
+	'notitle-desc' => '增加一个用以隐藏页面开头标题的魔术字',
+);
+
+/** Traditional Chinese (中文（繁體）‎)
+ * @author Simon Shek
+ */
+$messages['zh-hant'] = array(
+	'notitle-desc' => '增加一個用以隱藏頁面開頭標題的魔術字',
+);
